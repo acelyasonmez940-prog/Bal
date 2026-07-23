128fb49403fb5dbedb60fd512a3c1154fcea7bcc
acelyasonmez940-prog <acelyasonmez940@gmail.com>
2026-07-23 14:03:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-b
+function send(){
+
+let input=document.getElementById("text");
+
+if(input.value.trim()=="") return;
+
+let chat=document.getElementById("chat");
+
+chat.innerHTML+=`
+<div class="message user">
+${input.value}
+</div>
+`;
+
+input.value="";
+
+chat.scrollTop=chat.scrollHeight;
+
+// Yazıyor animasyonu
+
+let typing=document.createElement("div");
+
+typing.className="message bal";
+
+typing.id="typing";
+
+typing.innerHTML="🐝 Bal yazıyor...";
+
+chat.appendChild(typing);
+
+chat.scrollTop=chat.scrollHeight;
+
+setTimeout(()=>{
+
+typing.remove();
+
+chat.innerHTML+=`
+<div class="message bal">
+🐝 Seni dinliyorum 💗<br>
+Bana biraz daha anlatır mısın? 🍯
+</div>
+`;
+
+chat.scrollTop=chat.scrollHeight;
+
+},1500);
+
+}
