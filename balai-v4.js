bf2fffde52b5bde4de9527620b3a30818d162c40
acelyasonmez940-prog <acelyasonmez940@gmail.com>
2026-08-03 01:59:05 +0300

    Update balai-v4.js

--- balai-v4.js
+++ balai-v4.js
@@ -2,12 +2,37 @@ console.log("BAL V4 ÇALIŞTI");
 
 const aci = document.getElementById("userAci");
 const bolatinyo = document.getElementById("userBolatinyo");
+const welcomeScreen = document.getElementById("welcomeScreen");
+const chatScreen = document.getElementById("chatScreen");
+const messages = document.getElementById("messages");
+const headerStatus = document.getElementById("headerStatus");
 
-alert("JS Yüklendi");
+function addMessage(type, text) {
+    messages.innerHTML += `
+        <div class="message ${type}">
+            ${text}
+        </div>
+    `;
+}
+
+function selectUser(name) {
+    headerStatus.textContent = `💛 ${name} ile sohbet ediyor`;
+
+    welcomeScreen.style.display = "none";
+    chatScreen.style.display = "flex";
+
+    messages.innerHTML = "";
+
+    addMessage(
+        "bal",
+        `🐝 Merhaba <b>${name}</b> 💛<br><br>Ben Bal! Bugün nasılsın?`
+    );
+}
+
 aci.addEventListener("click", () => {
-    alert("Açı butonuna basıldı!");
+    selectUser("Açı");
 });
 
 bolatinyo.addEventListener("click", () => {
-    alert("Bolatinyo butonuna basıldı!");
+    selectUser("Bolatinyo");
 });
