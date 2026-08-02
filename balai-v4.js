a273902ec80e1789ed0984b7421f52b561421527
acelyasonmez940-prog <acelyasonmez940@gmail.com>
2026-08-03 01:57:43 +0300

    Update balai-v4.js

--- balai-v4.js
+++ balai-v4.js
@@ -4,3 +4,10 @@ const aci = document.getElementById("userAci");
 const bolatinyo = document.getElementById("userBolatinyo");
 
 alert("JS Yüklendi");
+aci.addEventListener("click", () => {
+    alert("Açı butonuna basıldı!");
+});
+
+bolatinyo.addEventListener("click", () => {
+    alert("Bolatinyo butonuna basıldı!");
+});
